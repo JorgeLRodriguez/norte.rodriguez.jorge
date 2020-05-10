d98ebf7454809a6f85963d77a75669f851a9628b
Jorge Rodriguez <jorge-11@live.com.ar>
2020-05-10 18:38:26 -0300

    se agrego tablas avanzadas

--- LPPA/WebSiteLPPA/Controllers/HTMLController.cs
+++ LPPA/WebSiteLPPA/Controllers/HTMLController.cs
@@ -13,5 +13,9 @@ namespace WebSiteLPPA.Controllers
         {
             return View();
         }
+        public ActionResult TablasAvanzadas()
+        {
+            return View();
+        }
     }
 }
