459702d68a1972bcb3d76542a9c2f819db792e3c
Jorge Rodriguez <jorge-11@live.com.ar>
2020-05-10 18:23:18 -0300

    Se agrego HTML-TABLAS

---
+++ LPPA/WebSiteLPPA/Controllers/HTMLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteLPPA.Controllers
+{
+    public class HTMLController : Controller
+    {
+        // GET: HTML
+        public ActionResult Tablas()
+        {
+            return View();
+        }
+    }
+}
